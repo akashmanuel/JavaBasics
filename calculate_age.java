9a646aaba62c7f352b6b37fba67bb58e14d8f298
Aiswarya Raju <aiswaryaraju2001@gmail.com>
2020-10-01 00:40:06 +0530

    Add files via upload

---
+++ calculate_age.java
@@ -0,0 +1,23 @@
+package mod2;
+import java.util.*;
+
+public class DOB {
+
+	public static void main(String[] args) {
+		
+		int a= 2020;
+		int c;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter year of birth");
+		int age = sc.nextInt();
+		c = a - age;
+		System.out.println("Age is "+c);
+		
+		
+		
+		
+		
+
+	}
+
+}
